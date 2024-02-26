5cd46873a253938f68a60593d69504743a64e959
Raymond Ballo <raymond.ballo.networking@gmail.com>
2024-02-26 17:19:15 +0100

    Add backward and forward

--- ts/Rover.ts
+++ ts/Rover.ts
@@ -1,7 +1,7 @@
 export default class Rover {
-    private _position : [Number, Number];
+    private _position : [number, number];
     private _orientation : "North" | "South" | "East" | "West";
-    constructor(position : [Number, Number], orientation : "North"|"South"|"East"|"West"){
+    constructor(position : [number, number], orientation : "North"|"South"|"East"|"West"){
         this._position = position;
         this._orientation = orientation;
     } 
@@ -12,5 +12,35 @@ export default class Rover {
 
     get position(){
         return this._position;
+    }
+
+    moveForward() {
+        if(this._orientation === "North"){
+            this._position[1] += 1;
+        }
+        if(this._orientation === "South"){
+            this._position[1] -= 1;
+        }
+        if(this._orientation === "West"){
+            this._position[0] -= 1;
+        }
+        if(this._orientation === "East"){
+            this._position[0] += 1;
+        }
+    }
+
+    moveBackward() {
+        if(this._orientation === "North"){
+            this._position[1] -= 1;
+        }
+        if(this._orientation === "South"){
+            this._position[1] += 1;
+        }
+        if(this._orientation === "West") {
+            this._position[0] += 1;
+        }
+        if(this._orientation === "East"){
+            this._position[0] -= 1;
+        }
     }
 }
--- ts/rover.test.ts
+++ ts/rover.test.ts
@@ -18,3 +18,115 @@ describe('Rover not moving', () => {
     expect(rover.orientation).toStrictEqual("North")
   })
 })
+
+/**
+ * Given : rover is at 0,0 ; facing North and move forward
+ * When move forward
+ * Then : return 0,1
+ */
+
+describe('Rover is moving', () => {
+  const rover = new Rover([0,0], "North");
+  test('forward', () => {
+    rover.moveForward();
+    expect(rover.position).toStrictEqual([0,1])
+  })
+})
+
+/**
+ * Given : rover is at 0,0 ; facing South and move forward
+ * When move forward
+ * Then : return 0,-1
+ */
+
+describe('Rover is moving', () => {
+  const rover = new Rover([0,0], "South");
+  test('forward', () => {
+    rover.moveForward();
+    expect(rover.position).toStrictEqual([0,-1])
+  })
+})
+
+/**
+ * Given : rover is at 0,0 ; facing East and move forward
+ * When move forward
+ * Then : return 1,0
+ */
+
+describe('Rover is moving', () => {
+  const rover = new Rover([0,0], "East");
+  test('forward', () => {
+    rover.moveForward();
+    expect(rover.position).toStrictEqual([1,0])
+  })
+})
+
+/**
+ * Given : rover is at 0,0 ; facing West and move forward
+ * When move forward
+ * Then : return -1,0
+ */
+
+describe('Rover is moving', () => {
+  const rover = new Rover([0,0], "West");
+  test('forward', () => {
+    rover.moveForward();
+    expect(rover.position).toStrictEqual([-1,0])
+  })
+})
+
+/**
+ * Given : rover is at 0,0 ; facing North and move backward
+ * When move backward
+ * Then : return 0,-1
+ */
+
+describe('Rover is moving', () => {
+  const rover = new Rover([0,0], "North");
+  test('backward', () => {
+    rover.moveBackward();
+    expect(rover.position).toStrictEqual([0,-1])
+  })
+})
+
+/**
+ * Given : rover is at 0,0 ; facing South and move backward
+ * When move backward
+ * Then : return 0,-1
+ */
+
+describe('Rover is moving', () => {
+  const rover = new Rover([0,0], "South");
+  test('backward', () => {
+    rover.moveBackward();
+    expect(rover.position).toStrictEqual([0,1])
+  })
+})
+
+/**
+ * Given : rover is at 0,0 ; facing East and move backward
+ * When move backward
+ * Then : return -1,0
+ */
+
+describe('Rover is moving', () => {
+  const rover = new Rover([0,0], "East");
+  test('backward', () => {
+    rover.moveBackward();
+    expect(rover.position).toStrictEqual([-1,0])
+  })
+})
+
+/**
+ * Given : rover is at 0,0 ; facing West and move backward
+ * When move backward
+ * Then : return -1,0
+ */
+
+describe('Rover is moving', () => {
+  const rover = new Rover([0,0], "West");
+  test('backward', () => {
+    rover.moveBackward();
+    expect(rover.position).toStrictEqual([1,0])
+  })
+})
